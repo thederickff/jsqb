a8d2ac7af6c1dc6d2e151487d7b0f54fb59734ae
Pablo Eduardo Martinez <pablo980629@hotmail.com>
2023-01-02 13:51:29 -0600

    Improve query builder

--- src/main/java/com/github/derickfelix/Jsqb.java
+++ src/main/java/com/github/derickfelix/Jsqb.java
@@ -15,146 +15,163 @@
  */
 package com.github.derickfelix;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * @author derickfelix    
+ * @author derickfelix
  */
 public class Jsqb {
 
-    private List<Table> tables;
-    private String where;
-    private String orderBy;
-    
-    private void initialize()
-    {
-        this.tables = new ArrayList<>();
-        this.where = null;
-        this.orderBy = null;
+  private List<Table> tables;
+  private List<String> where;
+  private List<String> fields;
+  private HashMap<String, String> parameters;
+  private String orderBy;
+  private boolean firstWhere;
+  private Pattern pattern = Pattern.compile("WHERE :([a-z0-9])+", Pattern.CASE_INSENSITIVE);
+
+  public static enum JOIN {
+    INNER(" INNER JOIN "), LEFT(" LEFT JOIN "), RIGHT(" RIGHT JOIN ");
+
+    public String joinOpt;
+
+    private JOIN(String joinOpt) {
+      this.joinOpt = joinOpt;
     }
-    
-    public Jsqb select(String tableName, String... fields)
-    {
-        initialize();
-        
-        Table table = new Table();
-        table.name = tableName;
-        table.fields = fields;
-        tables.add(table);
+  };
 
-        return this;
-    }
+  private void initialize() {
+    this.tables = new ArrayList<>();
+    this.where = new ArrayList<>();
+    this.fields = new ArrayList<>();
+    this.parameters = new HashMap<>();
+    this.where = null;
+    this.orderBy = null;
+    this.firstWhere = true;
+  }
 
-    public Jsqb innerJoin(String tableName, String on, String... fields)
-    {
-        Table table = new Table();
-        table.name = tableName;
-        table.fields = fields;
-        table.on = on;
-        tables.add(table);
+  public Jsqb select(String tableName, String... fields) {
+    initialize();
 
-        return this;
-    }
+    Table table = new Table();
+    table.name = tableName;
+    this.fields.addAll(Arrays.asList(fields));
+    tables.add(table);
 
-    public Jsqb where(String where)
-    {
-        this.where = where;
+    return this;
+  }
 
-        return this;
-    }
-    
-    public Jsqb orderBy(String orderBy, boolean descending)
-    {
-        this.orderBy = descending ? orderBy + " DESC" : orderBy;
-        
-        return this;
-    }
+  public Jsqb addSelect(String... fields) {
+    this.fields.addAll(Arrays.asList(fields));
+    return this;
+  }
 
-    public String write()
-    {
-        if (tables.get(0).name != null) {
-            StringBuilder sql = new StringBuilder();
-            sql.append("SELECT ");
+  public Jsqb join(JOIN join, String tableName, String on, String... fields) {
+    Table table = new Table();
+    table.join = join.joinOpt;
+    table.name = tableName;
+    table.on = on;
+    tables.add(table);
 
-            boolean all = true;
+    return this;
+  }
 
-            for (int i = 0; i < tables.size(); i++) {
-                Table table = tables.get(i);
-
-                if (table.fields.length != 0) {
-                    all = false;
-                    break;
-                }
-            }
-
-            if (all) {
-                Table from = tables.get(0);
-                sql.append(from.name).append(".* ");
-            } else {
-                for (int i = 0; i < tables.size(); i++) {
-                    Table table = tables.get(i);
-
-                    if (tables.size() == i + 1) {
-                        fillWithFields(table, sql, true);
-                    } else {
-                        fillWithFields(table, sql, false);
-                    }
-                }
-            }
-
-            for (int i = 0; i < tables.size(); i++) {
-                Table table = tables.get(i);
-
-                if (i == 0) {
-                    sql.append("FROM ")
-                            .append(table.name);
-                } else {
-                    sql.append(" INNER JOIN ")
-                            .append(table.name)
-                            .append(" on ")
-                            .append(table.on);
-                }
-            }
-
-            if (where != null) {
-                sql.append(" WHERE ").append(where);
-            }
-            
-            if (orderBy != null) {
-                sql.append(" ORDER BY ").append(orderBy);
-            }
-
-            return sql.toString();
-        }
-
-        return "";
+  public Jsqb where(String where, String... parameters) {
+    if (!this.firstWhere) {
+      List<String> parameterToRemove = getParametersFromWhere();
+      filterParameter(parameterToRemove);
     }
 
-    private void fillWithFields(Table table, StringBuilder sql, boolean last)
-    {
-        String[] fields = table.fields;
+    firstWhere = false;
+    where.concat(where);
 
-        for (int i = 0; i < fields.length; i++) {
-            String field = fields[i];
+    return this;
+  }
 
-            sql.append(table.name)
-                    .append('.')
-                    .append(field);
+  public Jsqb andWhere(String where, String... parameters) {
+    where = this.firstWhere ? where : "AND" + where;
+    where.concat(where);
+    firstWhere = false;
+    return this;
+  }
 
-            if (last && fields.length == i + 1) {
-                sql.append(" ");
-            } else {
-                sql.append(", ");
-            }
-        }
+  public void addParam(String column, String value) {
+    this.parameters.put(column, value);
+  }
+
+  public List<String> getParametersFromWhere() {
+    ArrayList<String> p = new ArrayList<String>();
+    for (String w : this.where) {
+      Matcher m = this.pattern.matcher(w);
+      if (m.find())
+        p.add(m.group(0));
+    }
+    return p;
+  }
+
+  private void filterParameter(List<String> parameterToRemove) {
+    parameterToRemove.parallelStream().forEach(p -> this.parameters.remove(p));
+  }
+
+  public Jsqb orderBy(String orderBy, boolean descending) {
+    this.orderBy = descending ? orderBy + " DESC" : orderBy;
+    return this;
+  }
+
+  public String write() {
+    if (tables.get(0).name != null)
+      return "Not valid sql";
+
+    StringBuilder sql = new StringBuilder();
+    sql.append("SELECT ");
+
+    boolean all = fields.size() > 1;
+
+    if (all) {
+      sql.append("* ");
+    } else {
+      fillWithFields(sql, true);
     }
 
-    private class Table {
+    sql.append("FROM ").append(tables.get(0).name);
 
-        private String name;
-        private String[] fields;
-        private String on;
-
+    for (int i = 1; i < tables.size(); i++) {
+      Table table = tables.get(i);
+      sql.append(table.join)
+          .append(table.name)
+          .append(" on ")
+          .append(table.on);
     }
+
+    where.forEach(w -> sql.append(w));
+
+    if (orderBy != null) {
+      sql.append(" ORDER BY ").append(orderBy);
+    }
+
+    return sql.toString();
+  }
+
+  private void fillWithFields(StringBuilder sql, boolean last) {
+    int lastElement = this.fields.size() - 1;
+    for (int i = 0; i < lastElement; i++) {
+      String field = this.fields.get(i);
+
+      sql.append(field).append(", ");
+    }
+
+    sql.append(fields.get(lastElement)).append(" ");
+  }
+
+  private class Table {
+    private String name;
+    private String on;
+    private String join;
+
+  }
 }
